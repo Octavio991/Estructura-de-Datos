fba372a2ab1cb6c547347db795ac08a814bbb085
Octavio991 <61813541+Octavio991@users.noreply.github.com>
2020-03-12 21:12:52 -0600

    Update prueba.java

--- prueba.java
+++ prueba.java
@@ -1,28 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author labsycapc13
- */
 public class prueba {
     
     public static void main(String args[]){
         
         complejo a = new complejo(3,4);
-        complejo b = new complejo(1,2);
-        complejo c = new complejo(1,-1);
+        complejo b = new complejo(1,3);
         
         System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(complejo.conjugado(c));
+        System.out.println(b);    
+        System.out.println(a.real());
+        System.out.println(a.imaginaria());
+        System.out.println(complejo.conjugado(a));        
         System.out.println(a.angulo());
+        System.out.println(complejo.division(a,b));
         System.out.println(complejo.suma(a,b));
         System.out.println(complejo.resta(a,b));
         System.out.println(complejo.producto(a,b));
+        System.out.println(a.magnitud());
+
+        
     }
 }
